b806f456b21aca4b888a3a40fb4bef528f945bff
Jeremy Yiu <jeremyy@usc.edu>
2023-04-04 11:33:16 -0700

    Sched initial complete

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleWork(DailySchedule& sched, const size_t workerCount, const size_t maxShifts);
+bool checkShiftValid(const size_t workerCount, const size_t maxShifts, const DailySchedule& sched);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,10 +37,49 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
+    
+    if(dailyNeed*avail.size() > maxShifts*avail[0].size()){return false;}
+    
+    sched = std::vector<std::vector<Worker_T>>(avail.size());
+    for(int i = 0;i<avail.size();i++){
+        std::vector<Worker_T> temp(dailyNeed);
+        for(int j = 0;j<dailyNeed;j++){
+            temp[j] = INVALID_ID;
+        }
+        sched[i] = temp;
+    }
 
-
+    return scheduleWork(sched, avail.size(), maxShifts);
 
 
 }
 
+bool checkShiftValid(const size_t workerCount, const size_t maxShifts, const DailySchedule& sched){
+    std::vector<int> counter(workerCount);
+    for(int i = 0;i<workerCount;i++){counter[i] = 0;}
+    for(int i = 0;i<sched.size();i++){
+        for(int j = 0;j<sched[i].size();j++){
+            counter[sched[i][j]]++;
+        }
+    }
+    for(int i = 0;i<workerCount;i++){
+        if(counter[i] > maxShifts){return false;}
+    }
+    return true;
+}
+
+bool scheduleWork(DailySchedule& sched, const size_t workerCount, const size_t maxShifts){
+    for(int i = 0;i<sched.size();i++){
+        for(int j = 0;j<sched[0].size();j++){
+            if(sched[i][j] == INVALID_ID){
+                for(int k = 0;k<workerCount;k++){
+                    sched[i][j] = k;
+                    if(scheduleWork(sched, workerCount, maxShifts)){return true;}
+                }
+                sched[i][j] = INVALID_ID;
+                return false;
+            }
+        }
+    }
+    return checkShiftValid(workerCount, maxShifts, sched);
+}
