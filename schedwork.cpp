dc0e151147b74584441d7a04446cf6b550355453
Jeremy Yiu <jeremyy@usc.edu>
2023-04-04 13:43:47 -0700

    sched remake 3

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,8 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-bool scheduleWork(DailySchedule& sched, const size_t maxShifts, const AvailabilityMatrix& avail);
-bool checkSingleValid(const Worker_T target, const size_t maxShifts, const DailySchedule& sched);
+bool scheduleWork(DailySchedule& sched, vector<int>& shiftCounter, const size_t maxShifts, const AvailabilityMatrix& avail);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -39,41 +38,35 @@ bool schedule(
     sched.clear();
     
     if(dailyNeed*avail.size() > maxShifts*avail[0].size()){return false;}
+    vector<int> shiftCounter(sched[0].size());
     
     sched = std::vector<std::vector<Worker_T>>(avail.size());
     for(int i = 0;i<avail.size();i++){
         std::vector<Worker_T> temp(dailyNeed);
         for(int j = 0;j<dailyNeed;j++){
             temp[j] = INVALID_ID;
+            shiftCounter[j] = 0;
         }
         sched[i] = temp;
     }
 
-    return scheduleWork(sched, maxShifts, avail);
+    return scheduleWork(sched, shiftCounter, maxShifts, avail);
 
 
 }
-
-bool checkSingleValid(const Worker_T target, const size_t maxShifts, const DailySchedule& sched){
-    int count = 0;
-    for(int i = 0;i<sched.size();i++){
-        for(int j = 0;j<sched[i].size();j++){
-            if(sched[i][j] == target){count++;}
-        }
-    }
-    return count <= maxShifts;
-}
-
-bool scheduleWork(DailySchedule& sched, const size_t maxShifts, const AvailabilityMatrix& avail){
+  
+bool scheduleWork(DailySchedule& sched, vector<int>& shiftCounter, const size_t maxShifts, const AvailabilityMatrix& avail){
     for(int i = 0;i<sched.size();i++){
         for(int j = 0;j<sched[0].size();j++){
             if(sched[i][j] == INVALID_ID){
                 for(int k = 0;k<avail[0].size();k++){
                     if(avail[i][k] == 1 && find(sched[i].begin(), sched[i].end(), k) == sched[i].end()){
                         sched[i][j] = k;
-                        if(checkSingleValid(k, maxShifts, sched)){
-                         if(scheduleWork(sched, maxShifts, avail)){return true;}   
+                        shiftCounter[k]++;
+                        if(shiftCounter[k] <= maxShifts){
+                         if(scheduleWork(sched, shiftCounter, maxShifts, avail)){return true;}   
                         }
+                        shiftCounter[k]--;
                     }
                 }
                 sched[i][j] = INVALID_ID;
