221e0e6798a9e04109e14dc18f7c49cc8c10e03b
Jeremy Yiu <jeremyy@usc.edu>
2023-04-04 13:37:18 -0700

    sched remake 2

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,7 +22,6 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 bool scheduleWork(DailySchedule& sched, const size_t maxShifts, const AvailabilityMatrix& avail);
-bool vectorContains(std::vector<Worker_T> vec, Worker_T target);
 bool checkSingleValid(const Worker_T target, const size_t maxShifts, const DailySchedule& sched);
 
 // Add your implementation of schedule() and other helper functions here
@@ -70,7 +69,7 @@ bool scheduleWork(DailySchedule& sched, const size_t maxShifts, const Availabili
         for(int j = 0;j<sched[0].size();j++){
             if(sched[i][j] == INVALID_ID){
                 for(int k = 0;k<avail[0].size();k++){
-                    if(avail[i][k] == 1 && !vectorContains(sched[i], k)){
+                    if(avail[i][k] == 1 && find(sched[i].begin(), sched[i].end(), k) == sched[i].end()){
                         sched[i][j] = k;
                         if(checkSingleValid(k, maxShifts, sched)){
                          if(scheduleWork(sched, maxShifts, avail)){return true;}   
@@ -83,10 +82,5 @@ bool scheduleWork(DailySchedule& sched, const size_t maxShifts, const Availabili
         }
     }
     return true;
-}
-
-bool vectorContains(std::vector<Worker_T> vec, Worker_T target){
-    for(int i = 0;i<vec.size();i++){if(vec[i] == target){return true;}}
-    return false;
 }
 
